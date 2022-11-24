f3ba1910c9a623e700011ee01dc020169c08993d
prasa <prasa@LAPTOP-H0PSVFQQ>
2022-11-24 22:22:06 +0530

    experiment10 commit

---
+++ src/main/java/Sonar573/AcoeSonarPip573/App.java
@@ -0,0 +1,18 @@
+package Sonar573.AcoeSonarPip573;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static int sum(int a,int b){
+    	return (a+b);
+    }
+    public static void main( String[] args )
+    {
+    	int var=sum(25,215);
+        System.out.println( "Hello World!" );
+        System.out.print(var);
+    }
+}
